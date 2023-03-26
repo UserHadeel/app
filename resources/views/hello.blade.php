a37e7abdc6964a642a176191f47dc61bb9d744af
github_username <you@example.com>
2023-03-27 01:16:28 +0300

    app

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en" >
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Student info</title>
+        <style>
+            table,tr,td {
+                border: 1px solid black;
+                direction: rtl;
+            }
+       </style>
+    </head>
+    <body>
+        <h3>البيانات الشخصية للطالبة هديل</h3>
+        <table>
+            <tr>
+                <td>الأسم</td>
+                <td>هديل علي كعيبير</td>
+            </tr>
+            <tr>
+                <td>تاريخ الميلاد</td>
+                <td>1/1/1999</td>
+            </tr>
+            <tr>
+                <td> البريد الألكتروني</td>
+                <td>hadilj99@gmail.com</td>
+            </tr>
+            <tr>
+                <td>الكلية</td>
+                <td>تقنية المعلومات</td>
+            </tr>
+            <tr>
+                <td>التخصص</td>
+                <td>هندسة البرمجيات</td>
+            </tr>
+        </table>
+
+    </body>
+</html>
+
